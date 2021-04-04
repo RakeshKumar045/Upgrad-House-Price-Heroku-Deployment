9da27a2acac2fc109ea8006f6d548022da698158
GitHubUser <raka06184@gmail.com>
2021-04-04 14:40:41 +0530

    added flask jupyter notebook files.

--- request.py
+++ request.py
@@ -1,6 +1,9 @@
 import requests
 
 url = 'http://localhost:5000/house_pred'
-res = requests.post(url, json={'experience': 2, 'test_score': 9, 'interview_score': 6})
+
+json_body_value_for_model = {"sqft": 1500, "place": 2, "yearsOld": 5, "totalFloor": 10, "bhk": 3}
+
+res = requests.post(url, json=json_body_value_for_model)
 
 print("Model Predicted Result : ", res.json())
